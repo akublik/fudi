39d79cab5728d7d2ef3fe437b92ef6b65c5ce150
André Kublik <info@estudionet.net>
2025-08-28 13:44:59 +0000

    no lo veo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="icon" href="https://imgur.com/5H2pCKQ.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&display=swap" rel="stylesheet" />
