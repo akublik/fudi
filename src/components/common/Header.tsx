bdef38623562f90f5402b6f6aad055970d01e8c4
André Kublik <info@estudionet.net>
2025-08-29 18:31:05 +0000

    cambiemos el logo por este: https://imgur.com/soZkYAE

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
         {/* Image on the left */}
         <div className="relative w-48 h-48 shrink-0">
           <Image
-            src="https://i.imgur.com/Oz9xver.png"
+            src="https://i.imgur.com/soZkYAE.png"
             alt="Fudo Logo"
             fill
             className="object-contain"
