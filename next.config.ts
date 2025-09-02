f0e5e42be428927ad5bfc69a6843d50717e3415e
André Kublik <info@estudionet.net>
2025-09-02 19:57:03 +0000

    puedo subir el favicono a alguna carpeta  para que quede visualmente en

--- next.config.ts
+++ next.config.ts
@@ -16,14 +16,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/icon.png',
-        destination: 'https://i.imgur.com/soZkYAE.png',
-      },
-    ]
-  },
 };
 
 export default nextConfig;
