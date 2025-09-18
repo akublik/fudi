bc665c4e08dea0c45105629acc5d70c852e26d3e
André Kublik <info@estudionet.net>
2025-09-18 14:26:28 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,36 +1,32 @@
-
 // Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-};
+// This script has access to the environment variables starting with NEXT_PUBLIC_
+firebase.initializeApp({
+    apiKey: self.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: self.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: self.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: self.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: self.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: self.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: self.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+});
 
-firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/icon.png",
+    icon: '/icon.png'
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
