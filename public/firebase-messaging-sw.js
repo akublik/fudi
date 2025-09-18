a8888e89e3f6260d957991b386c9404531b21311
André Kublik <info@estudionet.net>
2025-09-18 14:28:41 +0000

    sigue igual, no dañes mi aplicacion: Error: ./src/hooks/use-notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,26 @@
-// Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-// This script has access to the environment variables starting with NEXT_PUBLIC_
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
 firebase.initializeApp({
-    apiKey: self.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: self.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: self.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: self.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: self.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: self.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: self.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 });
 
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
