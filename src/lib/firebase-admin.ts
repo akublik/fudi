775e95949504aa3f5b9cc5ce6e5b02bb3803840f
André Kublik <info@estudionet.net>
2025-09-20 17:24:03 +0000

    sigue el mismo mnsaje, donde esta el problema?

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,5 @@
 
 import * as admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 // Re-implement a singleton pattern for Firebase Admin initialization.
 let app: admin.app.App | undefined;
